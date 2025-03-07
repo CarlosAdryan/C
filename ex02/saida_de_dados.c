a45b139933352ef0b51c257525bfd918ab490e3e
Carlos Adryan <132391014+CarlosAdryan@users.noreply.github.com>
2025-03-07 14:11:28 -0300

    Saida de Dados

--- ex02/saida_de_dados.c
+++ ex02/saida_de_dados.c
@@ -2,8 +2,11 @@
 
 int main()
 {
-    double x = (2.3456);
-    printf("%.2lf", x);
+    int idade = 32;
+    double salario = 4560.9;
+    char nome[50]   ;
+    strcpy(nome, "Maria Silva");
+    char sexo = 'F';
 
-    return 0;
+    printf("A funcionara %s, sexo %c, ganha %.2lf e tem %d anos", nome, sexo, salario, idade);
 }
